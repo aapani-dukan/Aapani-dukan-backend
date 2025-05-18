196c50e008fa4dc93b802bd8ae0f5e331e0cb57a
aapani-dukan <shivraj8404@gmail.com>
2025-05-19 02:22:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const multer = require('multer');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Server is live and working!");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
