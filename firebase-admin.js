edb62c382e73f23db2fe797c6820f3a5ebdfa4b8
aapani-dukan <shivraj8404@gmail.com>
2025-05-21 17:17:05 +0530

    Update firebase-admin.js

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,9 +1,8 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-key.json");
+const admin = require('firebase-admin');
+const serviceAccount = require('./firebase-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://aapani-dukan.firebaseio.com" // अगर realtime DB use कर रहे हों
 });
 
 module.exports = admin;
