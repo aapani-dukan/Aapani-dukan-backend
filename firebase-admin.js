894bfd5ca4ef8a9a36dd3df0dca150b724d32d5e
aapani-dukan <shivraj8404@gmail.com>
2025-05-21 18:12:20 +0530

    Update firebase-admin.js

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,8 +1,13 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-key.json');
+// firebase-admin.js
+const admin = require("firebase-admin");
+const atob = require("atob");
+
+const firebaseConfigBase64 = process.env.FIREBASE_CONFIG_BASE64;
+const firebaseConfigString = Buffer.from(firebaseConfigBase64, 'base64').toString('utf-8');
+const serviceAccount = JSON.parse(firebaseConfigString);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
