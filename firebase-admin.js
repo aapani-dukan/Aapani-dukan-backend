aeb9939aeefa16f6d0232e0b310b2452c6cee85d
aapani-dukan <shivraj8404@gmail.com>
2025-05-21 12:22:07 +0530

    Update firebase-admin.js

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,12 +1,10 @@
-const admin = require("firebase-admin");
-require('dotenv').config();
+const admin = require('firebase-admin');
+
+// .env से JSON स्ट्रिंग लेकर उसे JSON ऑब्जेक्ट में बदलना
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
