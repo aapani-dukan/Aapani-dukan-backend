30c42a4a09187e5fcff4df040d6c22d1c18fbaaf
aapani-dukan <shivraj8404@gmail.com>
2025-05-21 20:53:18 +0530

    Create passport-setup.js

---
+++ config/passport-setup.js
@@ -0,0 +1,14 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+    // अभी simple user object return कर रहे हैं
+    done(null, {
+        id: profile.id,
+        email: profile.emails[0].value
+    });
+}));
