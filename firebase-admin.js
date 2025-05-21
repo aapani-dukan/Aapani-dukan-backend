d489c2b0ea29a8d8ee5f3b3870710918c63aecf6
aapani-dukan <shivraj8404@gmail.com>
2025-05-21 09:07:30 +0530

    Update firebase-admin.js

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,6 +1,9 @@
-// firebase-admin.js
-const admin = require("firebase-admin");
-const serviceAccount = require(process.env.FIREBASE_KEY_PATH);
+const admin = require('firebase-admin');
+const fs = require('fs');
+
+const serviceAccount = JSON.parse(
+  fs.readFileSync(process.env.FIREBASE_KEY, 'utf8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
