d26c836fc3c1971d35b347e0182a12af6cbb93fa
aapani-dukan <shivraj8404@gmail.com>
2025-05-26 10:48:21 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,12 +5,12 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 // STEP 1: Redirect to Google
-router.get('/auth/google', passport.authenticate('google', {
+router.get('/google', passport.authenticate('google', {
   scope: ['profile', 'email'],
 }));
 
-// STEP 2: Google redirects to this after login (MUST MATCH Google Console)
-router.get('/auth/google/callback',
+// STEP 2: Callback
+router.get('/google/callback',
   passport.authenticate('google', { session: false }),
   (req, res) => {
     const user = req.user;
@@ -21,8 +21,7 @@ router.get('/auth/google/callback',
       role: user.role || 'customer',
     }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
-    // Redirect to frontend with token
-    const redirectUrl =`https://aapani-dukan-frontend-4444.vercel.app/CustomerDashboard?token=${token}`;
+    const redirectUrl = `https://aapani-dukan-frontend-4444.vercel.app/CustomerDashboard?token=${token}`;
     res.redirect(redirectUrl);
   }
 );
