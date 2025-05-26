8f189c5eb17ce9a8a429e65c131affc7b636ab9d
aapani-dukan <shivraj8404@gmail.com>
2025-05-26 09:48:34 +0530

    Update passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://aapani-dukan-backend-11.onrender.com/auth/callback"
+    callbackURL: "https://aapani-dukan-backend-11.onrender.com/auth/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
     // अभी simple user object return कर रहे हैं
     done(null, {
